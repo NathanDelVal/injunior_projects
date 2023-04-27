876d1f79a2fc7e96fee42a7c8beda6a90eca2cfe
NathanMad <nathan@internit.com.br>
2023-04-27 13:44:53 -0300

    add swagger

--- node/ignite-template-introducao-ao-SOLID/src/index.ts
+++ node/ignite-template-introducao-ao-SOLID/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require("./swagger.json");
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/users", usersRoutes);
 
 export { app };
